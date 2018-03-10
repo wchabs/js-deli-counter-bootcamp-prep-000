e1cf04de95680de416620079883067019dc060e5
William Chabla <w_chabla@hotmail.com>
2018-03-10 01:09:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var serving;
     serving = katzDeliLine[0];
-    katzDeliLine.pop();
+    katzDeliLine.shift();
     return `Currently serving ${serving}.`;
   } else
   return "There is nobody waiting to be served!";

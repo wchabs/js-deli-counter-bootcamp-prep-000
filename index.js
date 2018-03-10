d36e1d046732fbd0822222a80d5a23d4c1364dbe
William Chabla <w_chabla@hotmail.com>
2018-03-10 00:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `"Hello ${name}, you're ${katzDeliLine.length} in line."`;
+  return `"Welcome, ${name}. You are number ${katzDeliLine.length} in line."`;
 }
 
-function nowServing(katzDeliLine) {
+/*function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var serving;
     katzDeliLine.slice(1);
     return serving;
   } else
   return "There is nobody waiting to be servbed!";
-}
+}*/
 

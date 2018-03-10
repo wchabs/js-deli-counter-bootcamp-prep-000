6bdcb7f9f76402531ebc8402a555b9b6668c54d5
William Chabla <w_chabla@hotmail.com>
2018-03-10 00:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var katzDeli = [];
+
 function takeANumber (KatzDeliLine, name) {
-  katzDeliLine.push(name);
+  katzDeli.push(name);
   return `"Hello ${name}, you're ${katsDeli.length} in line."`;
 }
 

43aa546022f9374b28c8bdda16a56c2f43c586f9
William Chabla <w_chabla@hotmail.com>
2018-03-10 01:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function takeANumber (katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-/*function nowServing(katzDeliLine) {
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var serving;
     katzDeliLine.slice(1);
     return serving;
   } else
   return "There is nobody waiting to be servbed!";
-}*/
+}
 
